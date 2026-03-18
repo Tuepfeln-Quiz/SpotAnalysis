1ad5c4417e924aa1f201b28d19b1dacabce47d39
Smax Laptop <home.maxi.schmidt@gmail.com>
2026-03-18 15:16:02 +0100

    Created pages for quizzes (Sandbox, Reverse, Light, Tüpfeln)

---
+++ SpotAnalysis.Web/Models/ChemicalViewModel.cs
@@ -0,0 +1,13 @@
+namespace SpotAnalysis.Web.Models;
+
+public class ChemicalViewModel
+{
+    public int ChemicalID { get; set; }
+    public string Name { get; set; } = "";
+    public string Formula { get; set; } = "";
+    public string Color { get; set; } = "#cccccc";
+    public string? ImagePath { get; set; }
+    public int ChemicalTypeID { get; set; }
+    public string ChemicalTypeName { get; set; } = "";
+    public Dictionary<string, string> MethodOutputs { get; set; } = new();
+}

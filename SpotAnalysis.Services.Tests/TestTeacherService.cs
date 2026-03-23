6bc8dc41b50fc72dc3155ca4dc5f605ed5e69fdd
SoundAccordion04 <nima.hazrati@gmail.com>
2026-03-23 14:44:42 +0100

    create TestTeacherService.cs

---
+++ SpotAnalysis.Services.Tests/TestTeacherService.cs
@@ -0,0 +1,29 @@
+﻿using SpotAnalysis.Services.DTOs;
+using SpotAnalysis.Services.Services;
+
+namespace SpotAnalysis.Services.Tests;
+
+public class TestTeacherService : BaseDatabaseTest
+{
+    protected ITeacherService TeacherService;
+
+    [OneTimeSetUp]
+    public void InitTeacherService()
+    {
+        TeacherService = new TeacherService(ContextFactory);
+    }
+    
+    [Test]
+    public async Task TestTeacherCreateGroup()
+    {
+        await TeacherService.CreateGroup(1, new ConfigGroupDto
+        {
+            Name = "Test Group",
+            Description = "Test description"
+        });
+
+        var groups = await TeacherService.GetGroups(1);
+        
+        Assert.That(groups.Any(g => g.Name == "Test Group"));
+    }
+}

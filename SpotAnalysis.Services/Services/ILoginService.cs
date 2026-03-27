34921e8c5b6dd2ea794367a159d13e8b14ac28ee
MyShyGuy <matthias.schober.con@gmail.com>
2026-03-27 11:33:45 +0100

    null exeptions vermieden

--- SpotAnalysis.Services/Services/ILoginService.cs
+++ SpotAnalysis.Services/Services/ILoginService.cs
@@ -5,5 +5,5 @@ namespace SpotAnalysis.Services.Services;
 public interface ILoginService
 {
     public User? Login(string userName, string password);
-    public User ChangePassword(string userName, string oldPassword, string newPassword);
+    public User? ChangePassword(string userName, string oldPassword, string newPassword);
 }

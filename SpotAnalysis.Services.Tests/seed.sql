76abb318a80f36656957d070e919bb30b64ed67c
SoundAccordion04 <nima.hazrati@gmail.com>
2026-03-27 09:55:48 +0100

    add roles to seed script

--- SpotAnalysis.Services.Tests/seed.sql
+++ SpotAnalysis.Services.Tests/seed.sql
@@ -5,3 +5,12 @@ INSERT INTO dbo.Users (UserName, UserID, PasswordHash) VALUES
     ('Schueler1', 3, '$ghi'),
     ('Schueler2', 4, '$jkl');
 SET IDENTITY_INSERT dbo.Users OFF 
+
+SET IDENTITY_INSERT dbo.Roles ON
+INSERT INTO dbo.Roles (RoleID, Title) VALUES
+    (1, 'teacher'),
+    (2, 'student');
+SET IDENTITY_INSERT dbo.Roles OFF
+
+INSERT INTO RoleUser (RoleID, UserID) VALUES
+    (1, 1);

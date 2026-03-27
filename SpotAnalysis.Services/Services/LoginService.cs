d74bd2e305a9f97ca231f19eb6c10917bdefb8db
MyShyGuy <matthias.schober.con@gmail.com>
2026-03-27 09:19:29 +0100

    Login done.

--- SpotAnalysis.Services/Services/LoginService.cs
+++ SpotAnalysis.Services/Services/LoginService.cs
@@ -1,20 +1,39 @@
 
 namespace SpotAnalysis.Services.Services;
 
-public class LoginService : ILoginService , IStudentService
+using System.Security.Cryptography.X509Certificates;
+using SpotAnalysis.Data;
+using SpotAnalysis.Data.Models.Identity;
+
+public class LoginService : ILoginService 
 {
-    public void ChangePassword(string userName, string oldPassword, string newPassword)
+    private readonly AnalysisContext _context;
+    private readonly string Salt = "S0m3R@nd0mS@lt!Because_I-Don't_Want_You_To_Crack_My_Passwords_and_i-Dont-wanna-add.a_GUID_as_salt";
+
+    public LoginService(AnalysisContext context)
+    {
+        _context = context;
+    }
+
+    public User ChangePassword(string userName, string oldPassword, string newPassword)
     {
         System.Console.WriteLine("ChangePassword method called with username: " + userName + ", old password: " + oldPassword + " and new password: " + newPassword);
+        return null;
     }
 
-    public void Login(string userName, string password)
+    public User? Login(string userName, string password)
     {
         System.Console.WriteLine("Login method called with username: " + userName + " and password: " + password);
-    }
 
-    public void Register(string password, string? code)
-    {
-        System.Console.WriteLine("Register method called with password: " + password + " and code: " + code);
+        var user = _context.Users.FirstOrDefault(u => u.UserName == userName);
+        if (user == null) return null;
+
+        var hashedPassword = new ArgonProvider.ArgonOutput(password, Salt);
+        var storedHash = ArgonProvider.ArgonOutput.FromParamString(user.PasswordHash);
+        if (hashedPassword.Compare(storedHash))
+        {
+            return user;
+        }
+        return null;
     }
 }

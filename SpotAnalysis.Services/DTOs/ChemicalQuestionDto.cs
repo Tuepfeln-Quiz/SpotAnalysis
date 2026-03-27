d3bde365ce49cb0d127a24e6637e9abb36d1abd7
Luca Liebhart <Luca.Liebhart@liebherr.com>
2026-03-27 11:30:58 +0100

    Resolve comments

--- SpotAnalysis.Services/DTOs/ChemicalQuestionDto.cs
+++ SpotAnalysis.Services/DTOs/ChemicalQuestionDto.cs
@@ -4,7 +4,7 @@ public class ChemicalQuestionDto
 {
     public required int Id { get; init; }
     public required string Name { get; init; }
-    public required string Formular { get; init; }
+    public required string Formula { get; init; }
     public required string Color { get; init; }
     public required bool IsAdditive { get; init; }
 }
--- SpotAnalysis.Services/DTOs/STLQuestionDto.cs
+++ SpotAnalysis.Services/DTOs/STLQuestionDto.cs
@@ -4,6 +4,7 @@ public class STLQuestionDto
 {
     public required int Id { get; init; }
     public required string Description { get; init; }
+    public required int Order { get; init; }
     public required ChemicalDto Educt { get; init; }
     public required string Observation { get; init; }
 }

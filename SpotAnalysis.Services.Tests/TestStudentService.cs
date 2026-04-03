7aadf160a35ede50e6a0e6d301324351bbff6c1b
SoundAccordion04 <nima.hazrati@gmail.com>
2026-04-03 17:01:33 +0200

    barebones student service test

---
+++ SpotAnalysis.Services.Tests/TestStudentService.cs
@@ -0,0 +1,24 @@
+﻿#define PASSWORD_PROVIDER_DETERMINISTIC
+
+using SpotAnalysis.Services.Services;
+
+namespace SpotAnalysis.Services.Tests;
+
+public class TestStudentService : BaseDatabaseTest
+{
+#pragma warning disable CA1859
+    private IStudentService _studentService;
+#pragma warning restore CA1859
+    
+    [OneTimeSetUp]
+    public void InitStudentService()
+    {
+        _studentService = new StudentService(ContextFactory);
+    }
+
+    [Test]
+    public async Task TestAllStudentService()
+    {
+        
+    }
+}

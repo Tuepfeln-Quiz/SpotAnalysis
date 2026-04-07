7b1eeda6df215b193e3ab74e3c47266b45d13c72
SoundAccordion04 <nima.hazrati@gmail.com>
2026-04-07 14:55:01 +0200

    optional guid parameter

--- SpotAnalysis.Services/Services/IStudentService.cs
+++ SpotAnalysis.Services/Services/IStudentService.cs
@@ -4,7 +4,7 @@ namespace SpotAnalysis.Services.Services;
 
 public interface IStudentService
 {
-    public Task Register(string userName, string password, string? email);
+    public Task Register(string userName, string password, string? email, Guid? userId);
     public Task<List<QuizOverviewDto>> GetQuizzes(Guid studentId);
     public Task<QuizDto> OpenQuiz(Guid studentId, int quizId);
     public Task ValidateAndSaveQuestion(ValAndSaveQuestionDto questionResult);

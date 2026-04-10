e313462014816a100fa3efef71f73c17f05e6409
Luca Liebhart <Luca.Liebhart@liebherr.com>
2026-04-10 09:02:32 +0200

    cherry pick quiz service interface

---
+++ SpotAnalysis.Services/Services/IQuizService.cs
@@ -0,0 +1,11 @@
+﻿using SpotAnalysis.Services.DTOs;
+
+namespace SpotAnalysis.Services.Services;
+
+public interface IQuizService
+{
+    public List<QuizDto> GetQuizzes();
+    public void CreateQuiz(ConfigQuizDto quiz);
+    public void UpdateQuiz(ConfigQuizDto quiz);
+    public void DeleteQuiz(int quizId);
+}

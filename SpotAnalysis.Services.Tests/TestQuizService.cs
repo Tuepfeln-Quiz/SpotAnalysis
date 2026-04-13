41771b86c1e528ae3c3b58949cb414ef6fa5eb6c
Luca Liebhart <Luca.Liebhart@liebherr.com>
2026-04-13 15:50:51 +0200

    Implement and test assigning groups to quizzes

--- SpotAnalysis.Services.Tests/TestQuizService.cs
+++ SpotAnalysis.Services.Tests/TestQuizService.cs
@@ -54,7 +54,7 @@ public class TestQuizService : BaseDatabaseTest
         {
             Id = 10,
             Name = newQuizName,
-            Questions = [],
+            Questions = []
         };
         
         await _quizService.UpdateQuiz(_createdBy, quizToUpdate);
@@ -214,7 +214,7 @@ public class TestQuizService : BaseDatabaseTest
             tasks.Add(_quizService.CreateQuiz(_createdBy, new CreateQuizDto
             {
                 Name = i.ToString(),
-                Questions = [],
+                Questions = []
             }));
         }
 

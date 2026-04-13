029dd86650c6c2a9b3a160cdf8e2eaa0310f9d4b
antoniaknapp <antoniaknapp@LAPTOP-1IGIPC09>
2026-04-13 11:15:24 +0200

    add class descriptions

--- SpotAnalysis.Data/Models/Quizzes/STAvailableChemical.cs
+++ SpotAnalysis.Data/Models/Quizzes/STAvailableChemical.cs
@@ -1,6 +1,10 @@
 ﻿namespace SpotAnalysis.Data.Models.Quizzes;
 
 
+/// <summary>
+/// Contains the chemicals that need to be identified in the specified SpotTestQuestion.
+/// </summary>
+
 [PrimaryKey(nameof(QuestionID), nameof(ChemicalID))] 
 public class STAvailableChemical {
     public int QuestionID { get; set; }

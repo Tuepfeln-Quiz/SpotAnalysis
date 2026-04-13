f3d255b88ca7700bf933c0c0c2c3022a2efd0720
antoniaknapp <antoniaknapp@LAPTOP-1IGIPC09>
2026-04-13 16:16:50 +0200

    updated delete behaviours

--- SpotAnalysis.Data/AnalysisContext.cs
+++ SpotAnalysis.Data/AnalysisContext.cs
@@ -62,19 +62,25 @@ public class AnalysisContext : DbContext {
         .ToTable(t => t.HasCheckConstraint("CK_Reaction_ChemicalOrder", "\"Chemical1ID\" <= \"Chemical2ID\""));
 
         modelBuilder.Entity<User>()
-        .HasMany(u => u.Groups)
-        .WithMany(r => r.Users)
-        .UsingEntity(
-            u => u.HasOne(typeof(Group)).WithMany().HasForeignKey("GroupID").OnDelete(DeleteBehavior.Restrict),
-            g => g.HasOne(typeof(User)).WithMany().HasForeignKey("UserID").OnDelete(DeleteBehavior.Restrict)
+            .HasMany(u => u.Groups)
+            .WithMany(r => r.Users)
+            .UsingEntity(
+                // deleting a group is only possible, when it is not referenced by any user
+                groupForeignKey => groupForeignKey
+                    .HasOne(typeof(Group)).WithMany().HasForeignKey("GroupID").OnDelete(DeleteBehavior.Restrict),
+                // deleting the user also deletes all references to the user in the join table, but not the groups themselves
+                userForeignKey => userForeignKey
+                    .HasOne(typeof(User)).WithMany().HasForeignKey("UserID").OnDelete(DeleteBehavior.Cascade)
             );
 
         modelBuilder.Entity<Quiz>()
         .HasMany(q => q.Groups)
         .WithMany(g => g.Quizzes)
         .UsingEntity(
+            // deleting a quiz is only possible, when it is not referenced by any group
             q => q.HasOne(typeof(Group)).WithMany().HasForeignKey("GroupID").OnDelete(DeleteBehavior.Restrict),
+            // deleting a group is only possible, when it is not referenced by any quiz
             g => g.HasOne(typeof(Quiz)).WithMany().HasForeignKey("QuizID").OnDelete(DeleteBehavior.Restrict)
-            );
+         );
     }
 }

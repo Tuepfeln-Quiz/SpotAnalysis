83835c00402af0e4d05e3e96fd693d95c3736740
Ali Minah (HAU-LFHA) <Minah.Ali@liebherr.com>
2026-04-17 10:04:27 +0200

    admin test

--- SpotAnalysis.Services.Tests/TestAdminService.cs
+++ SpotAnalysis.Services.Tests/TestAdminService.cs
@@ -2,6 +2,7 @@
 using NSubstitute;
 using SpotAnalysis.Data.Enums;
 using SpotAnalysis.Services.Services;
+using System.Linq;
 
 namespace SpotAnalysis.Services.Tests;
 
@@ -9,7 +10,7 @@ public class TestAdminService : BaseDatabaseTest
 {
     private AdminService _adminService;
     private IUserService _userService;
-    
+
     [OneTimeSetUp]
     public void InitAdminService()
     {
@@ -22,26 +23,26 @@ public class TestAdminService : BaseDatabaseTest
     public async Task AddRoleToUser_RemoveRoleFromUser()
     {
         await CleanUpDb();
-        
+
         await _userService.Register("RandomUser", "RandomPassword");
-        
+
         var user = await _userService.Login("RandomUser", "RandomPassword");
-        
+
         Assert.That(user, Is.Not.Null);
         Assert.That(user.Roles, Has.Count.EqualTo(1));
         Assert.That(user.Roles, Contains.Item(Role.Student));
 
         await _adminService.AddRoleToUser(user.UserID, Role.Teacher);
-        
+
         user = await _userService.Login("RandomUser", "RandomPassword");
-        
+
         Assert.That(user, Is.Not.Null);
         Assert.That(user.Roles, Has.Count.EqualTo(2));
         Assert.That(user.Roles, Contains.Item(Role.Teacher));
         Assert.That(user.Roles, Contains.Item(Role.Student));
-        
+
         await _adminService.RemoveRoleFromUser(user.UserID, Role.Teacher);
-        
+
         user = await _userService.Login("RandomUser", "RandomPassword");
         Assert.That(user, Is.Not.Null);
         Assert.That(user.Roles, Has.Count.EqualTo(1));
@@ -52,16 +53,45 @@ public class TestAdminService : BaseDatabaseTest
     public async Task DeleteUser()
     {
         await CleanUpDb();
-        
+
         await _userService.Register("RandomUser", "RandomPassword");
-        
+
         var user = await _userService.Login("RandomUser", "RandomPassword");
-        
-        
+
         Assert.That(user, Is.Not.Null);
-        
+
         await _adminService.DeleteUser(user.UserID);
         user = await _userService.Login("RandomUser", "RandomPassword");
         Assert.That(user, Is.Null);
+    }
+
+    [Test]
+    public async Task GetUsersByRole()
+    {
+        await CleanUpDb();
+
+        await _userService.Register("RandomUser", "RandomPassword");
+        var user = await _userService.Login("RandomUser", "RandomPassword");
+        Assert.That(user, Is.Not.Null);
+
+        var students = await _adminService.GetUsersByRole(Role.Student);
+        Assert.That(students, Is.Not.Empty);
+        Assert.That(students.All(x => x.Roles.Contains(Role.Student.ToString())), Is.True);
+
+        await _adminService.AddRoleToUser(user.UserID, Role.Admin);
+        user = await _userService.Login("RandomUser", "RandomPassword");
+        Assert.That(user, Is.Not.Null);
+        Assert.That(user.Roles, Contains.Item(Role.Admin));
+        var admins = await _adminService.GetUsersByRole(Role.Admin);
+        Assert.That(admins, Is.Not.Empty);
+        Assert.That(admins.All(x => x.Roles.Contains(Role.Admin.ToString())), Is.True);
+
+        await _adminService.AddRoleToUser(user.UserID, Role.Teacher);
+        user = await _userService.Login("RandomUser", "RandomPassword");
+        Assert.That(user, Is.Not.Null);
+        Assert.That(user.Roles, Contains.Item(Role.Teacher));
+        var teachers = await _adminService.GetUsersByRole(Role.Teacher);
+        Assert.That(teachers, Is.Not.Empty);
+        Assert.That(teachers.All(x => x.Roles.Contains(Role.Teacher.ToString())), Is.True);
     }
 }

0f8e9887f8b43f9e50aa68887eda31f5c833353b
Luca Liebhart <Luca.Liebhart@liebherr.com>
2026-04-17 11:54:38 +0200

    Remove unused Dto and change admin overview

--- SpotAnalysis.Services/DTOs/TeacherAdminDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpotAnalysis.Services.DTOs;
-
-public class TeacherAdminDto
-{
-    public required Guid Id { get; init; }
-    public required string UserName { get; init; }
-    public required List<string> Roles { get; init; } = [];
-}
